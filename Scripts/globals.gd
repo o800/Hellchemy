873329d595a3e3ad3f2147fbbdf4dcf4b6f97232
o800 <151675302+o800@users.noreply.github.com>
2026-08-07 13:16:55 +1200

    commit

--- Scripts/globals.gd
+++ Scripts/globals.gd
@@ -1,9 +1,11 @@
 extends Node
 var json = JSON.new()
+var discovered_recipes: Dictionary
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	json.parse(FileAccess.open("res://Assets/recipes.json", FileAccess.READ).get_as_text())
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -20,5 +22,16 @@ func combine(item1: String, item2: String) -> Variant:
 	
 	if json.data.has(item1):
 		if json.data[item1].has(item2):
+			discovered_recipes[item1][item2] = true
 			return json.data[item1][item2]
 	return null
+	
+
+func is_recipe_discovered(item1, item2):
+	if item2 < item1:
+		var tmp = item2
+		item2 = item1
+		item1 = tmp
+	if discovered_recipes[item1][item2]:
+		return true	
+	return false
