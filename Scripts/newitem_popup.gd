6682252d40f89703e53475009df6814f51e16ee9
qq476-476 <phamnh@whs.school.nz>
2026-08-26 12:19:00 +1200

    h

--- Scripts/newitem_popup.gd
+++ Scripts/newitem_popup.gd
@@ -3,8 +3,12 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$TextureRect.self_modulate.a = 0
-	get_tree().create_tween().tween_property($TextureRect, "self_modulate:a", 1, 0.8)
+	for x in self.get_children():
+		x.self_modulate.a = 0
+	for x in self.get_children():
+		get_tree().create_tween().tween_property(x, "self_modulate:a", 1, 1)
+	await get_tree().create_timer(0.8 + 1.5).timeout
+	get_tree().change_scene_to_file("res://Scenes/main.tscn")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
